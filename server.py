ddadf7d1e5125c0d5d80111b3891e56133979527
stephaniewang26 <159806328+stephaniewang26@users.noreply.github.com>
2024-03-08 13:58:30 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,45 @@
+# flask --app data_server run
+from flask import Flask
+from flask import request
+from flask import render_template
+import json
+
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def index():
+    f = open("data/life_expectancy.json")
+    data = json.load(f)
+    f.close()
+
+    #check to see if year is in the query string portion of the URL
+    requested_year = request.args.get('year')
+    if requested_year == None:
+        requested_year = "2020" #just in case
+
+    countries = sorted(list(data.keys()))
+    years = sorted(list(data["Canada"].keys()))
+    yearsby10 = []
+    for i in range(0,len(years),10):
+        yearsby10.append(years[i])
+    line_endpoints =[]
+    print(years)
+    # #Filter and reformat data for ease of access in the template
+    # requested_data = data[requested_pet]
+    # years = sorted(list(requested_data.keys()))
+    # line_endpoints =[]
+    # for i in range(len(years)-1): # make it easy to dynamically generate a line graph
+    #     start_x = years[i] #generate endpoints for each line segment
+    #     stop_x = years[i+1]
+    #     line_endpoints.append([requested_data[start_x],requested_data[stop_x]] )
+
+    # return render_template('individual_scores.html', pet=requested_pet, years = years, endpoints = line_endpoints)
+
+    return render_template('index.html',year=requested_year,all_years=years,year_increment = yearsby10)
+
+@app.route('/year')
+def year():
+    return render_template('year.html')
+
+app.run(debug=True)
